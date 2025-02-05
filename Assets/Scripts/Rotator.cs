d7d57c5f2d882031b78f72453fcb4407a8963c01
Bernard <W0453579@campus.nscc.ca>
2025-02-05 12:25:08 -0400

    Added obstacles to level 1

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 rotateAmount;
+
+    void Update()
+    {
+        transform.Rotate(rotateAmount * Time.deltaTime);
+    }
+}
